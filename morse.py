c5636d8bcc8c20d8dfe8a49bdf78e6e8d946bb7d
Ben Salter <>
2020-12-13 10:08:22 +0000

    Fix typo in morse, and update docstrings

--- morse.py
+++ morse.py
@@ -1,21 +1,18 @@
-"""Placeholder Morse docstring"""
+"""Morse
+
+The Morse module consists of a flashing light that repeats a word in
+Morse code, and a display to set a frequency.
+"""
 
 from colours import bold
 from utils import get_input
 
-def get_morse_input():
-    while True:
-        morse_sequence = get_input("Input a morse code letter (. = dot, - = dash): ")
-        if morse_sequence in ("EXIT", "QUIT"):
-            return None
-
-        valid_morse = (all(char in ".-" for char in morse_sequence) and
-                       1 <= len(morse_sequence) <= 4)
-        if valid_morse:
-            return morse_sequence
-        print("Invalid morse sequence. Please try again")
-
 class Morse:
+    """Class to represent the Morse module. Solving requires getting
+    the Morse characters appearing on the light to decode the Morse into
+    a word. Each word has a corresponing frequency, which must be
+    inputted and transmitted.
+    """
     VALID_WORDS = {"SHELL":  3.505,
                    "HALLS":  3.515,
                    "SLICK":  3.522,
@@ -25,7 +22,7 @@ class Morse:
                    "STROBE": 3.545,
                    "BISTRO": 3.552,
                    "FLICK":  3.555,
-                   "BOMBS":  3.656,
+                   "BOMBS":  3.565,
                    "BREAK":  3.572,
                    "BRICK":  3.575,
                    "STEAK":  3.582,
@@ -39,21 +36,34 @@ class Morse:
                      "--.-": "Q", ".-.": "R", "...": "S", "-": "T",
                      "..-": "U", "...-": "V", ".--": "W", "-..-": "X",
                      "-.--": "Y", "--..": "Z"}
-    def solve(self):
-        """Solves the morse module. The user inputs morse characters until
-        there is only one valid word left.
+
+    def get_morse_input(self):
+        """Prompt a user to input a single Morse code letter, and return
+        a single character representing that letter.
         """
+        while True:
+            morse_sequence = get_input("Input a morse code letter (. = dot, - = dash): ")
+            if morse_sequence in ("EXIT", "QUIT"):
+                return None
+
+            try:
+                return self.MORSE_LETTERS[morse_sequence]
+            except KeyError:
+                print("Invalid morse sequence. Please try again")
+
+    def solve(self):
+        """Solves the Morse module."""
         # Make a copy to remove words from when ruled out.
         valid_words = dict(self.VALID_WORDS)
         while len(valid_words) > 1:
-            morse_sequence = get_morse_input()
-            if morse_sequence is None:  # "EXIT" or "QUIT"
+            morse_character = self.get_morse_input()
+            if morse_character is None:  # "EXIT" or "QUIT"
                 return
 
             # Make a copy to iterate over, since we modify valid_words
             valid_words_iter = dict(valid_words)
             for word in valid_words_iter:
-                if self.MORSE_LETTERS[morse_sequence] not in word:
+                if morse_character not in word:
                     del valid_words[word]
 
         # Now we have at most one valid word
