037d1fef4ca80da2743c31e59e3a066d283c7ba9
Ben Salter <>
2020-12-13 10:08:37 +0000

    Added docstrings to needy knob

--- needy_knob.py
+++ needy_knob.py
@@ -1,3 +1,10 @@
+"""Needy Knob
+
+The Needy Knob module is a needy module that consists of a knob with
+four possible directions, and a series of lights and an UP indicator to
+indicate how to position the knob.
+"""
+
 from colours import bold
 from utils import get_input
 
@@ -5,6 +12,9 @@ VALID_LIGHTS = {"44": "UP", "43": "UP", "53": "DOWN", "32": "DOWN",
                 "14": "LEFT", "03": "LEFT", "54": "RIGHT", "52": "RIGHT"}
 
 def get_knob_input():
+    """Prompt user for the number of lights lit on the left/right sides
+    sides of the module, and return it as a string.
+    """
     while True:
         lights = get_input("\nInput the number of lit lights on the left hand side, "
                            "and the number on the right: ")
@@ -20,8 +30,13 @@ def get_knob_input():
                 return normalised_lights
 
 class NeedyKnob:
+    """Class to represent the Needy Knob module. Solving requires
+    getting the light arrangement, and based off that, turning the knob
+    to a specific position relative to the "UP" indicator.
+    """
     def __init__(self):
         self.lights = get_knob_input()
 
     def solve(self):
+        """Solve the Needy Knob module."""
         print(f'Turn knob {bold(VALID_LIGHTS[self.lights])} relative to "UP"')
